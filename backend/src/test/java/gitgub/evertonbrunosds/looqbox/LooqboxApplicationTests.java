21e273ae3ac0253a21fc5162a50f4cefdf11d33d
evertonbrunosds <evertonbrunosds@gmail.com>
2024-12-29 16:44:49 -0300

    feat/install-springboot-framework

---
+++ backend/src/test/java/gitgub/evertonbrunosds/looqbox/LooqboxApplicationTests.java
@@ -0,0 +1,13 @@
+package gitgub.evertonbrunosds.looqbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LooqboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
