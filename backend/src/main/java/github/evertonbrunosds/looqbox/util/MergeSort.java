f88dcdeea7de41ac1cf6778199fc5512fa5f9944
evertonbrunosds <evertonbrunosds@gmail.com>
2024-12-30 07:56:11 -0300

    perf: change quick sort by merge sort

--- backend/src/main/java/github/evertonbrunosds/looqbox/util/MergeSort.java
+++ backend/src/main/java/github/evertonbrunosds/looqbox/util/MergeSort.java
@@ -10,7 +10,9 @@ public class MergeSort<T> {
     private final Comparer<T, T, Integer> comparer;
 
     public MergeSort() {
-        comparer = (final T o1, final T o2) -> compare(o1.hashCode(), o2.hashCode());
+        comparer = (final T leftElement, final T rightElement) -> {
+            return compare(leftElement.hashCode(), rightElement.hashCode());
+        };
     }
 
     public MergeSort(final Comparer<T, T, Integer> comparer) {
@@ -18,20 +20,24 @@ public class MergeSort<T> {
     }
 
     public void sort(final List<T> list, final boolean reverse) {
+        final Comparer<T, T, Boolean> comparerMode = reverse
+                ? (leftElement, rightElement) -> comparer.compare(leftElement, rightElement) > 0
+                : (leftElement, rightElement) -> comparer.compare(leftElement, rightElement) < 0;
+        split(list, comparerMode);
+    }
+
+    public void split(final List<T> list, final Comparer<T, T, Boolean> comparerMode) {
         if (list.size() > 1) {
             final int middle = list.size() / 2;
             final List<T> leftList = new ArrayList<>(list.subList(0, middle));
             final List<T> rightList = new ArrayList<>(list.subList(middle, list.size()));
-            sort(leftList, reverse);
-            sort(rightList, reverse);
-            perform(list, leftList, rightList, reverse);
+            split(leftList, comparerMode);
+            split(rightList, comparerMode);
+            merge(list, leftList, rightList, comparerMode);
         }
     }
 
-    private void perform(final List<T> list, final List<T> leftList, final List<T> rightList, final boolean reverse) {
-        final Comparer<T, T, Boolean> comparerMode = reverse
-                ? (paramOne, paramTwo) -> comparer.compare(paramOne, paramTwo) > 0
-                : (paramOne, paramTwo) -> comparer.compare(paramOne, paramTwo) < 0;
+    private void merge(final List<T> list, final List<T> leftList, final List<T> rightList, final Comparer<T, T, Boolean> comparerMode) {
         int leftIndex = 0, rightIndex = 0, index = 0;
         while (leftIndex < leftList.size() && rightIndex < rightList.size()) {
             if (comparerMode.compare(leftList.get(leftIndex), rightList.get(rightIndex))) {
