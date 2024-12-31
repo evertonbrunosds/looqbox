21f8f3be04998b24b8691ee759726e5d1b1af9f0
evertonbrunosds <evertonbrunosds@gmail.com>
2024-12-31 03:50:43 -0300

    perf: implements caching mechanism

---
+++ backend/src/main/java/github/evertonbrunosds/looqbox/util/Cache.java
@@ -0,0 +1,73 @@
+package github.evertonbrunosds.looqbox.util;
+
+import java.security.InvalidParameterException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Cache {
+
+    private Object data;
+
+    private LocalDateTime invalidAt;
+
+    private final long timeInterval;
+
+    private final Process<?> onUpdate;
+
+    private final MeasureTime measureTime;
+
+    public Cache(final long timeInterval, final MeasureTime measureTime, final Process<?> onUpdate) {
+        if (timeInterval <= 0) {
+            throw new InvalidParameterException("the 'timeInterval' parameter must be greater than zero");
+        }
+        this.data = null;
+        this.invalidAt = LocalDateTime.now();
+        this.timeInterval = timeInterval;
+        this.onUpdate = onUpdate;
+        this.measureTime = measureTime;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> Alternator<T> getData() {
+        final boolean updateData = Duration.between(invalidAt, LocalDateTime.now()).toNanos() >= 0;
+        try {
+            if (updateData) {
+                data = onUpdate.get();
+                switch (measureTime) {
+                    case SECOND:
+                        invalidAt = LocalDateTime.now().plusSeconds(timeInterval);
+                        break;
+                    case MINUTE:
+                        invalidAt = LocalDateTime.now().plusMinutes(timeInterval);
+                        break;
+                    case HOUR:
+                        invalidAt = LocalDateTime.now().plusHours(timeInterval);
+                        break;
+                }
+            }
+            final T convertedData = (T) data;
+            return (alternativeData) -> convertedData;
+        } catch (final Throwable throwable) {
+            return (alternativeData) -> alternativeData;
+        }
+    }
+
+    @FunctionalInterface
+    public interface Alternator<A> {
+
+        A orGet(A alternative);
+
+    }
+
+    public enum MeasureTime {
+        SECOND, MINUTE, HOUR
+    }
+
+    @FunctionalInterface
+    public interface Process<O> {
+
+        public O get() throws Throwable;
+
+    }
+
+}
