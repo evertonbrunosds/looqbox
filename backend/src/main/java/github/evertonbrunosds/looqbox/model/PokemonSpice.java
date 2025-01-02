c20e142ef338d534fcac1eaaa12aa8e6f4165c6f
evertonbrunosds <evertonbrunosds@gmail.com>
2025-01-02 03:28:24 -0300

    feat: implements PokemonSpice class

---
+++ backend/src/main/java/github/evertonbrunosds/looqbox/model/PokemonSpice.java
@@ -0,0 +1,24 @@
+package github.evertonbrunosds.looqbox.model;
+
+public class PokemonSpice {
+
+    private String name;
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(final String url) {
+        this.url = url;
+    }
+
+}
