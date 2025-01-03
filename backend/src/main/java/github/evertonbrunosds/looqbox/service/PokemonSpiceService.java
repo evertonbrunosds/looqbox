ade0d8ad7311a8482c936582333a9367aac5c38b
evertonbrunosds <evertonbrunosds@gmail.com>
2025-01-03 02:24:19 -0300

    refactor: cache into PokemonSpiceService class

--- backend/src/main/java/github/evertonbrunosds/looqbox/service/PokemonSpiceService.java
+++ backend/src/main/java/github/evertonbrunosds/looqbox/service/PokemonSpiceService.java
@@ -5,9 +5,7 @@ import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toList;
 import static org.springframework.util.StringUtils.hasText;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
@@ -23,28 +21,22 @@ public class PokemonSpiceService {
 
     private static final MeasureTime MEASURE_TIME = MINUTE;
 
-    private final Cache findAll;
-
-    private final Map<String, Cache> findByNameIgnoreCase;
+    private final PokemonSpiceRepository repository;
 
     public PokemonSpiceService(final PokemonSpiceRepository repository) {
-        findAll =  new Cache(TIME_INTERVAL, MEASURE_TIME, repository::findAll);
-        findByNameIgnoreCase = new HashMap<>();
+        final Cache cache = new Cache(TIME_INTERVAL, MEASURE_TIME, repository::findAll);
+        this.repository = () -> cache.<List<PokemonSpice>>getData().orGet(emptyList());
     }
 
     public List<PokemonSpice> findAll() {
-        return findAll.<List<PokemonSpice>>getData().orGet(emptyList()).stream().collect(toList());
+        return repository.findAll().stream().collect(toList());
     }
 
     public List<PokemonSpice> findByNameIgnoreCase(final String name) {
         return hasText(name)
-                ? findByNameIgnoreCase.computeIfAbsent(name.toLowerCase(), key -> {
-                    return new Cache(TIME_INTERVAL, MEASURE_TIME, () -> {
-                        return findAll().stream().filter(pokemonSpice -> {
-                            return pokemonSpice.getName().toLowerCase().contains(name.toLowerCase());
-                        }).collect(toList());
-                    });
-                }).<List<PokemonSpice>>getData().orGet(emptyList())
+                ? repository.findAll().stream().filter(pokemonSpice -> {
+                    return pokemonSpice.getName().toLowerCase().contains(name.toLowerCase());
+                }).collect(toList())
                 : emptyList();
     }
 
